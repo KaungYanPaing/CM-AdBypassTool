3c22d63275971b93347f39970101dba34a07213e
KaungYanPaing <51271827+KaungYanPaing@users.noreply.github.com>
2020-03-21 23:42:16 +0630

    Add files via upload

---
+++ Ad_Skipper.py
@@ -0,0 +1,42 @@
+import base64
+
+def main():
+	banner()
+	bypass()
+	end()
+	while True:
+		process = input('Do you want to continue (yes/no) :: ')
+
+		if (process == 'yes' or process == 'y' or process == 'Yes' or process == 'Y'):
+			bypass()
+			end()
+			continue
+		elif (process == 'no' or process == 'n' or process == 'No' or process == 'N'):
+			break
+		else:
+			print('Enter either "yes" or "no") :: ')
+	end()
+
+
+def banner():
+	print('\n')
+	print('\t ==--==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==--==')
+	print('\t ==-- Channel Myanmar Movie Ad Skipper --==')
+	print('\t ==--          Auther : T0x1c          --==')
+	print('\t ==--==--==--==--==--==--==--==--==--==--==')
+	print('\t ==--==--==--==--==--==--==--==--==--==--==')
+	print('\n')
+	print('\t eg - Enter your movie link :: https://www.roda.site/blah...')
+	print('\n')
+
+def bypass():
+	url = input('\nEnter your movie link :: ')
+	code = url.split("/?r=")[1]
+	output_link = base64.b64decode(code)
+	print('\n [#] Original Download Link is here :p >>> '+output_link.decode())
+	
+def end():
+	print('\n----------------------------------------------------------------------------------------------------------------------\n')
+
+if __name__ == '__main__':main()
